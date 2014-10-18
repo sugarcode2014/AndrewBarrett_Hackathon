01b04391f516626909fd031671ddf72e882e6e79
Andrew Barrett <sugarcod2014@gmail.com>
2014-10-18 14:08:56 -0300

    gui in progress
    2 pm commit

--- BottleRecycler/src/com/titanicapps/bottlerecycler/MainActivity.java
+++ BottleRecycler/src/com/titanicapps/bottlerecycler/MainActivity.java
@@ -1,7 +1,6 @@
 package com.titanicapps.bottlerecycler;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,19 +8,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
+	static int MIN_DENOM = 0;
+	static int MAX_DENOM = 99;
+	static int MAX_COUNT = 1000;
+	static int MIN_COUNT = 0;
+	
+	private int mDenom = 0;
+	private int mCount = 0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		if (savedInstanceState == null) {
+			
 			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+					.add(R.id.container, new PlaceholderFragment()).commit();		
+
 		}
+
 	}
 
 	@Override
@@ -59,6 +69,68 @@ public class MainActivity extends ActionBarActivity {
 					false);
 			return rootView;
 		}
+	}
+	
+	public void onDecreaseDenom(View view)
+	{
+		if(mDenom-- <= MIN_DENOM)
+		{
+			mDenom = MIN_DENOM;
+		}
+		String tmpText = String.valueOf(mDenom);
+		TextView denomText = (TextView) findViewById(R.id.txtDenom);		
+		if(denomText != null)
+		{
+			denomText.setText(tmpText);
+		}
+	}
+	
+	public void onIncreaseDenom(View view)
+	{
+		if(mDenom++ >= MAX_DENOM)
+		{
+			mDenom = MAX_DENOM;
+		}
+		
+		String tmpText = String.valueOf(mDenom);
+		TextView denomText = (TextView) findViewById(R.id.txtDenom);
+		if(denomText != null)
+		{
+			denomText.setText(tmpText);
+		}
+	}
+	
+	public void onIncreaseCount(View view)
+	{
+		if(mCount++ >= MAX_COUNT)
+		{
+			mCount = MAX_COUNT;
+		}
+		String tmpText = String.valueOf(mCount);
+		TextView countText = (TextView) findViewById(R.id.txtCount);		
+		if(countText != null)
+		{
+			countText.setText(tmpText);
+		}		
+	}
+	
+	public void onDecreaseCount(View view)
+	{
+		if(mCount-- <= MIN_COUNT)
+		{
+			mCount = MIN_COUNT;
+		}
+		String tmpText = String.valueOf(mCount);
+		TextView countText = (TextView) findViewById(R.id.txtCount);		
+		if(countText != null)
+		{
+			countText.setText(tmpText);
+		}		
+	}
+	
+	private String formatCount(int count)
+	{
+	
 	}
 
 }
